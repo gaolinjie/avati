a9f7311a847210a53f7740665203aca61959e778
gaolinjie <gaolinjie@gmail.com>
2014-11-03 23:59:00 +0800

    editor remove header1-6

--- static/summernote-0.5.2-dist/summernote.js
+++ static/summernote-0.5.2-dist/summernote.js
@@ -994,12 +994,6 @@
           normal: 'Normal',
           blockquote: 'Quote',
           pre: 'Code',
-          h1: 'Header 1',
-          h2: 'Header 2',
-          h3: 'Header 3',
-          h4: 'Header 4',
-          h5: 'Header 5',
-          h6: 'Header 6'
         },
         lists: {
           unordered: 'Unordered list',
